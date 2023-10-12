38543eed7840fae85628e22a3b20c768bf75605a
Adrie Komaroff <adrienkomaroff@hotmail.fr>
2023-10-12 18:56:06 +0200

    initial commit

---
+++ include/stm32_to_ros/SerialID.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+namespace SerialID
+{
+	inline constexpr uint8_t MSG_ACK = 0xFF;
+	inline constexpr uint8_t MSG_START = 0xFE;
+	inline constexpr uint8_t MSG_NO_START = 0xFD;
+	inline constexpr uint8_t MSG_NO_ACK = 0xFC;
+	inline constexpr uint8_t MSG_NO_ID = 0xFB;
+	inline constexpr uint8_t MSG_NO_SIZE = 0xFA;
+	inline constexpr uint8_t MSG_PRINT = 0x00;
+	inline constexpr uint8_t MSG_DATA_1 = 0x01;
+};
